7f7f951b58f27d37caa376d2f3a1348e244ec8fc
sharontlin <sharonlin14@gmail.com>
2020-04-25 13:54:43 -0400

    Removed progress bar

--- question.js
+++ question.js
@@ -55,8 +55,7 @@ function populate() {
             element.innerHTML = choices[i];
             guess("btn" + i, choices[i]);
         }
- 
-        showProgress();
+
     }
 };
  
@@ -67,16 +66,10 @@ function guess(id, guess) {
         populate();
     }
 };
- 
-function showProgress() {
-    var currentQuestionNumber = quiz.questionIndex + 1;
-    var element = document.getElementById("progress");
-    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
-};
 
 function redoQuiz() {
     var element = document.getElementById("quiz");
-    element.innerHTML = "<h1>Unemployment Qualification Test</h1><hr style=\"margin-bottom: 20px\"><p id=\"question\"></p><div class=\"buttons\"><button id=\"btn0\"><span id=\"choice0\"></span></button><button id=\"btn1\"><span id=\"choice1\"></span></button></div><hr style=\"margin-top: 50px\"><footer><p id=\"progress\">Question x of y</p></footer>";
+    element.innerHTML = "<h1>Unemployment Qualification Test</h1><hr style=\"margin-bottom: 20px\"><p id=\"question\"></p><div class=\"buttons\"><button id=\"btn0\"><span id=\"choice0\"></span></button><button id=\"btn1\"><span id=\"choice1\"></span></button></div>";
 
     quiz = new Quiz(questions);
     populate();
@@ -101,7 +94,7 @@ function notQualified() {
 var questions = [
     new Question("Did you leave your job voluntarily?", ["Yes", "No"], "Yes", "Maybe", "You will need to be reviewed by a claims examiner through a fact-finding interview. Your employer may also be contacted and the examineer will determine your eligibility."),
     new Question("Were you fired for misconduct?", ["Yes", "No"], "Yes", "Maybe", "You will need to be reviewed by a claims examiner through a fact-finding interview. Your employer may also be contacted and the examineer will determine your eligibility."),
-    new Question("Do you meet the minimum earnings requirement of $10k during your regular base year period (first four of the last five completed calendar quarters) or $200/week for 20 weeks? Alternatively, you can evaluate your earning against the four most recently completed calendar quarters preceding the date of the claim or the three most recently completed calendar quarters preceding the date of claim, and weeks and wages in the filing quarter up to your last day of work (this will contain fewer than 52 weeks).", ["Yes", "No"], "No", "Unqualified", "If you don't meet the minimum earnings requirement, you do not qualify for unemployment benefits."),
+    new Question("Do you meet the minimum earnings requirement of $10k during (a) your regular base year period (first four of the last five completed calendar quarters) or $200/week for 20 weeks, (b) the four most recently completed calendar quarters preceding the date of the claim, or (c) the three most recently completed calendar quarters preceding the date of claim, and weeks and wages in the filing quarter up to your last day of work.", ["Yes", "No"], "No", "Unqualified", "If you don't meet the minimum earnings requirement, you do not qualify for unemployment benefits."),
     new Question("Are you a teacher or school employee?", ["Yes", "No"], "Yes", "Maybe", "If you have a claimed filed curing a recess period, only school wages in the base period of the claim, or an offer to return to work for a school employer when the recess period ends, you may not be eligibile, although eligibility depends on your unique situation."),
     new Question("Are you a corporate officer or business owner?", ["Yes", "No"], "Yes", "Maybe", "If you own more than a 5 percent equitable (you own more than 5% of the capital stock of the corporation, either by yourself or with your spouse) or debt interest and your claim is based on wages with the corproation, you will not be considered unemployed during your term of office or ownership."),
     new Question("Are you self-employed, a part-time worker, or an independent contractor?", ["Yes", "No"], "Yes", "Qualified", "You may qualify for unemployment under the national coronavirus releif act. You should apply for unemployment insurance."),
