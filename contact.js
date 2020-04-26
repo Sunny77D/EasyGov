6bdfaca2f6d9caa69a0b6837e4383bf53bf91240
Victor Hua <vhua@princeton.edu>
2020-04-26 13:46:21 -0400

    initial contact page

---
+++ contact.js
@@ -0,0 +1,18 @@
+function Message(name, email, message) {
+    this.name = name;
+    this.email = email;
+    this.message = message;
+}
+
+$(document).ready(function() {
+    $('form').submit(function(e) {
+        e.preventDefault()
+        name = $('#name').val()
+        email = $('#email').val()
+        message = $('#message').val()
+        const contact = new Message(name, email, message)
+        console.log(contact)
+
+        $('#sent').html('<p>Your message has been sent!</p>')
+    });
+});
